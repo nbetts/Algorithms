217690d9d315d8e64cbbaa7b161ee12c68d95f8c
Nathan Betts <nathanbetts@hotmail.co.uk>
2018-05-09 20:06:58 +0100

    Add selection sort

--- code/src/main/java/Algorithms.java
+++ code/src/main/java/Algorithms.java
@@ -2,13 +2,17 @@
 public class Algorithms {
   private Algorithms() {}
 
+  private static <T> void swap(T[]array, int first, int second) {
+    T element = array[first];
+    array[first] = array[second];
+    array[second] = element;
+  }
+
   public static <T extends Comparable<? super T>> void bubbleSort(T[] array) {
     for (int i = 0; i < array.length - 1; i++) {
       for (int j = 0; j < array.length - 1; j++) {
         if (array[j].compareTo(array[j+1]) > 0) {
-          T element = array[j];
-          array[j] = array[j+1];
-          array[j+1] = element;
+          swap(array, j, j+1);
         }
       }
     }
@@ -18,10 +22,24 @@ public class Algorithms {
     for (int i = 1; i < array.length; i++) {
       for (int j = i; j > 0; j--) {
         if (array[j].compareTo(array[j-1]) < 0) {
-          T element = array[j];
-          array[j] = array[j-1];
-          array[j-1] = element;
+          swap(array, j, j-1);
         }
+      }
+    }
+  }
+
+  public static <T extends Comparable<? super T>> void selectionSort(T[] array) {
+    for (int i = 0; i < array.length - 1; i++) {
+      int lowest = i;
+
+      for (int j = i + 1; j < array.length; j++) {
+        if (array[j].compareTo(array[lowest]) < 0) {
+          lowest = j;
+        }
+      }
+
+      if (lowest != i) {
+        swap(array, i, lowest);
       }
     }
   }
@@ -32,7 +50,8 @@ public class Algorithms {
 
     ArrayUtilities.print(array);
     // Algorithms.bubbleSort(array);
-    Algorithms.insertionSort(array);
+    // Algorithms.insertionSort(array);
+    Algorithms.selectionSort(array);
     ArrayUtilities.print(array);
   }
 }
