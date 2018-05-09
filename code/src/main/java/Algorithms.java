d7a28b00b7a3ee303fab8f9725762ec8d8480bda
Nathan Betts <nathanbetts@hotmail.co.uk>
2018-05-09 19:08:17 +0100

    Add insertion sort

--- code/src/main/java/Algorithms.java
+++ code/src/main/java/Algorithms.java
@@ -14,11 +14,25 @@ public class Algorithms {
     }
   }
 
+  public static <T extends Comparable<? super T>> void insertionSort(T[] array) {
+    for (int i = 1; i < array.length; i++) {
+      for (int j = i; j > 0; j--) {
+        if (array[j].compareTo(array[j-1]) < 0) {
+          T element = array[j];
+          array[j] = array[j-1];
+          array[j-1] = element;
+        }
+      }
+    }
+  }
+
   public static void main(String[] args) {
-    Integer[] array = ArrayUtilities.generateRandomIntegerArray(20, 1, 30);
+    // Integer[] array = ArrayUtilities.generateRandomIntegerArray(20, 1, 30);
+    Integer[] array = ArrayUtilities.generateReverseIntegerArray(20);
 
     ArrayUtilities.print(array);
-    Algorithms.bubbleSort(array);
+    // Algorithms.bubbleSort(array);
+    Algorithms.insertionSort(array);
     ArrayUtilities.print(array);
   }
 }
