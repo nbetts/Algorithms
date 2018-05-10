38eee207f10d9b070d4a7e12ae3c952d0aec0fb9
Nathan Betts <nathanbetts@hotmail.co.uk>
2018-05-10 23:40:33 +0100

    Forgot to add updated main method

--- code/src/main/java/Main.java
+++ code/src/main/java/Main.java
@@ -1,5 +1,20 @@
 
 public class Main {
   public static void main(String[] args) throws Exception {
-    Algorithms.testAlgorithm("bubbleSort", false);
+    // Algorithms.testAlgorithm("bubbleSort", false);
+
+    PerformanceAnalyser analyser = new PerformanceAnalyser();
+
+    System.out.println("Warming up performance analyser...");
+    analyser.warmUp();
+    System.out.println("Warmed up");
+
+    System.out.format("Analysing performance of algorithm \"bubbleSort\"," +
+                      " running 20 tests using an array size of 1000 ...\n");
+
+    long averageExecutionTime = analyser.runAnalysis("bubbleSort", 10000, 20);
+
+    System.out.format("Analysis complete, average execution time: %s\n",
+                      PerformanceAnalyser.formatNanoTime(averageExecutionTime));
+  }
 }
